d25fa3813ec7516e91382d37123d34f6d3f21a3a
Hillary Murai <muraihillary@gmail.com>
2025-02-24 02:18:55 +0300

    commit

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const transporter = nodemailer.createTransport({
   service: "gmail", 
   auth: {
       user: process.env.EMAIL_USER, 
-      pass: process.env.EMAIL_PASS, // Your email password or app password
+      pass: process.env.EMAIL_PASS, 
   },
 });
 
