955260ac99ac8062bb132781c5eced032a90ade4
Hillary Murai <muraihillary@gmail.com>
2025-02-23 04:03:32 +0300

    commit

--- routes/driverRoutes.js
+++ routes/driverRoutes.js
@@ -9,6 +9,9 @@ router.post("/login_process", driverController.loginDriver);
 router.get("/dashboard", driverAuthMiddleware.isDriverLoggedIn, driverController.getDriverDashboard);
 
 // Password reset routes
+
+router.post("/forgot-password", driverController.forgotPassword); // Request password reset
+
 // Driver request routes
 router.get("/pending-requests", driverAuthMiddleware.isDriverLoggedIn, driverController.getPendingRequests);
 router.get("/resolve-request", driverAuthMiddleware.isDriverLoggedIn, driverController.resolveRequest);
